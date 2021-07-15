567fdc446e405dfc21412dcf6d969259c6c43013
chenhongqun <kyle.chen@celnet.com.cn>
2021-07-16 00:47:02 +0800

    测试

---
+++ src/main/java/com/chq/Test.java
@@ -0,0 +1,17 @@
+package com.chq;
+
+import java.util.Random;
+
+/**
+ * 项目名称:  Java
+ * 包:        com.chq
+ * 类名称:    Test
+ * 类描述:
+ * 创建人:    chq
+ * 创建时间:  2021/7/15 21:33
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello2");
+    }
+}
