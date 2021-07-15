722cf3a312fb25e168b6cfefb00adcca730f2f23
chenhongqun <kyle.chen@celnet.com.cn>
2021-07-16 00:50:15 +0800

    修改

--- src/main/java/com/chq/Test.java
+++ src/main/java/com/chq/Test.java
@@ -12,6 +12,6 @@ import java.util.Random;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello2");
+        System.out.println("Hello1");
     }
 }
